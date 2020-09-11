7e0e3b60d322b6a3fd9ecb219b4ec1843b107cab
Rachel Pike <rachelpike@rachels-mbp-2.hamilton.edu>
2020-09-11 17:04:04 -0400

    finished first pass at all questions. still unsure about a few

--- Activity 3.R
+++ Activity 3.R
@@ -23,3 +23,60 @@ bartlett.test(ch4$CH4_Flux ~ ch4$herbivory)
 t.test(ch4$CH4_Flux ~ ch4$herbivory)
 # p-value well above confidence threshold --> difference between means could be
 #   due to random chance --> don't reject null hypothesis
+
+# Example of a one-sided t test
+t.test(ch4$CH4_Flux, mu=10, alternative = 'greater')
+
+# Learning ANOVA testing
+
+# Reading in the insect data
+datI <- read.csv("/Users/rachelpike/Downloads/insect_richness.csv")
+
+# Making the names into factors
+datI$urbanName <- as.factor(datI$urbanName)
+
+# Checking to see if data is normally distributed
+shapiro.test(datI$Richness[datI$urbanType == 1])
+shapiro.test(datI$Richness[datI$urbanType == 3])
+shapiro.test(datI$Richness[datI$urbanType == 8])
+shapiro.test(datI$Richness[datI$urbanType == 9])
+
+# Checking to see if the variances are equal across groups
+bartlett.test(datI$Richness ~ datI$urbanType)
+
+# Specify model for species richness and urban type
+in.mod <- lm(datI$Richness ~ datI$urbanName)
+
+# Run the ANOVA
+in.aov <- aov(in.mod)
+
+# Print out ANOVA table
+summary(in.aov)
+
+#run Tukey HSD to test how different means are of the different groups
+tukeyT <- TukeyHSD(in.aov)
+
+#view results
+tukeyT
+
+# Make a plotof Tukey test
+# Make axes labels smaller than usual to fit on plot using cex.axis 
+plot(tukeyT, cex.axis=0.75)
+
+# Looking at the means of each group
+tapply(datI$Richness, datI$urbanName, "mean")
+
+# Learning Chi Squared Testing
+
+# Set up contigency table with data on protected species population behavior
+species <- matrix(c(18,8,15,32), ncol=2, byrow = TRUE) 
+colnames(species) <- c("Not protected", "Protected")
+rownames(species) <- c("Declining", "Stable/Increase")
+
+# Make a mosaic plot with an informative title and axes labels
+mosaicplot(species, xlab="population status", ylab="legal protection",
+           main="Legal protection impacts on populations")
+
+# Conduct a chi-squared test
+chisq.test(species)
+
