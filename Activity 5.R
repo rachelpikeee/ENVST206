f3c6e9f875ec9a7955223500e87072830e69a450
Rachel Pike <rachelpike@rachels-mbp-2.hamilton.edu>
2020-09-30 11:47:07 -0400

    finished activity but need to add comments in code

--- Activity 5.R
+++ Activity 5.R
@@ -3,6 +3,8 @@
 # Loading in the ggplot package (need to do everytime)
 library(ggplot2)
 
+library(lubridate)
+
 # Reading in data set
 datW <- read.csv("/Users/rachelpike/ENVST Data/noaa2011124.csv")
 
@@ -61,35 +63,32 @@ legend("topleft", #position
        lwd=1, #line thickness 1, anytime both point & line arguments are given both will be drawn
        bty="n") #always use this argument otherwise an ugly box is drawn
 
-# Adding a mean annual temp to datW
-datW$MAT <- (datW$TMAX + datW$TMIN)/2
-
 # Make a dataframe with just mean annual temp, year, and site name
 # Remove NA using na.omit
 datT <- na.omit(data.frame(NAME=datW$NAME,
                            year=datW$year,
-                           MAT=datW$MAT))
+                           TMAX=datW$TMAX))
 
-# Getting average annual mean temperatures (mm)
-mat <- aggregate(datT$MAT, by=list(datT$NAME,datT$year), FUN="mean", na.rm=TRUE)
+# Getting average annual mean max temperatures (mm)
+tmax <- aggregate(datT$TMAX, by=list(datT$NAME,datT$year), FUN="mean", na.rm=TRUE)
 
 # Changing column sames
-colnames(mat) <- c("NAME", "YEAR", "MAT")
+colnames(tmax) <- c("NAME", "YEAR", "TMAX")
 
 # Add the x column from aggregate looking at the length of observations in each year
 # This is to see how many observations we have per year
-mat$ncount <- aggregate(datT$MAT, by=list(datT$NAME,datT$year), FUN="length")$x
+tmax$ncount <- aggregate(datT$TMAX, by=list(datT$NAME,datT$year), FUN="length")$x
 
 # Subsetting data by how many observations we think is acceptable
 # No convention, just what we think is appropriate for the data
-mat <- mat[mat$ncount >= 364,]
+tmax <- tmax[tmax$ncount >= 364,]
 
 # Looking at only livermore california and morrisville new york preciptiation
-ND <- mat[mat$NAME == nameS[3], ]
-NY <- mat[mat$NAME == nameS[5], ]
+ND <- tmax[tmax$NAME == nameS[3], ]
+NY <- tmax[tmax$NAME == nameS[5], ]
 
 # Making a plot comparing New York and North Dakota mat
-plot(ND$YEAR, ND$MAT,
+plot(ND$YEAR, ND$TMAX,
      type = "o",
      pch = 19,
      col = "deepskyblue2",
@@ -97,11 +96,11 @@ plot(ND$YEAR, ND$MAT,
      xlab = "Year",
      xlim = c(1930, 2016),
      yaxt = "n",
-     ylim =c(2, 9))
+     ylim =c(8,16))
 #add y axis
-axis(2, seq(2,9, by=1), las=2 )
+axis(2, seq(8,16, by=2), las=2 )
 #add new york
-points(NY$YEAR, NY$MAT,
+points(NY$YEAR, NY$TMAX,
        type = "o",
        pch = 19,
        col="goldenrod1")
@@ -151,11 +150,80 @@ ggplot(data=sub, aes(x=DATE, y=TMAX))+
   geom_point()+
   geom_path()+
   theme_classic()+
-  labs(x="year", y="Maximimum temperature (C)")
+  labs(x="year", y="Maximimum temperature (˚C)")
 
-# Making bar plot for precip data
+# Making bar plot for AZ precip data
 ggplot(data=sub, aes(x=DATE, y=PRCP))+
   geom_col(fill="royalblue3")+
   theme_classic()+
   labs(x="year", y="Daily precipitation (mm)")
 
+# Looking at daily patterns in CA site in 1974
+CA <- datW[datW$NAME == nameS[2] & datW$ year == 1974,]
+
+# Specify date format
+# %Y means a four number year 
+# - indicates that the date uses dashes to seperate
+# %m means month
+# %d means day
+CA$DATE <- as.Date(CA$DATE,"%Y-%m-%d")
+
+# Making scatter plot for 1974 CA data
+ggplot(data=CA, aes(x=DATE, y=TMAX))+
+  geom_point()+
+  geom_path()+
+  theme_classic()+
+  labs(title = "Maximum Temperature at Livermore CA Site in 1974", 
+       x="year", y="Maximimum temperature (˚C)")
+
+# Making bar plot for CA precip data
+ggplot(data=CA, aes(x=DATE, y=PRCP))+
+  geom_col(fill="royalblue3")+
+  theme_classic()+
+  labs(x="year", y="Daily precipitation (mm)",
+       title = "Daily Precipitation at Livermore CA Site in 1974")
+
+# Installing lubridate package to look at the data by month
+install.packages("lubridate")
+
+# Looking just at the minimum temperatures at the Washington site for the past
+#   two decades
+WA <- datW[datW$NAME == nameS[1] & datW$ year >= 2000,]
+
+# Making the date column stored as a date
+WA$DATE <- as.Date(WA$DATE,"%Y-%m-%d")
+
+# Isolating just the month data
+WA$Month <- month(WA$DATE)
+
+# Trying to get the average per month per year --> something is not working here
+WAtmin <- aggregate(WA$TMIN, by=list(WA$year,WA$Month), FUN="mean", na.rm=TRUE)
+
+colnames(WAtmin) <- c("YEAR", "MONTH", "TMIN")
+
+# Getting the number of data per month
+WAtmin$ncount <- aggregate(WA$TMIN, by=list(WA$year,WA$Month), FUN="length")$x
+
+# Saying there must be at least 27 data points per month
+WAtmin <- WAtmin[WAtmin$ncount >= 27,]
+
+WAtmin$season <- ifelse((WAtmin$MONTH <=2 | WAtmin$MONTH >=12), "winter", 
+                        ifelse((WAtmin$MONTH > 5 & WAtmin$MONTH < 9), "summer", 
+                               ifelse((WAtmin$MONTH > 2 & WAtmin$MONTH < 6), "spring","fall")))
+
+
+WAtminszn <- aggregate(WAtmin$TMIN, by=list(WAtmin$YEAR,WAtmin$season), FUN="mean", na.rm=TRUE)
+
+colnames(WAtminszn) <- c("YEAR", "SEASON", "TMIN")
+
+
+# Making plot for min temps
+
+ggplot()+
+  geom_line(data = WAtminszn, aes(x=YEAR, y=TMIN, color = SEASON))+
+  theme_classic()+
+  labs(title = "Annual Average Minimum Temperature in Aberden, WA", 
+       x="Year", y="Minimum temperature (˚C)")+
+  scale_colour_manual(labels = c("Fall (SON)", "Spring (MAM)", "Summer (JJA)", "Winter (DJF)"), values = c("darkorange2", "forestgreen", "goldenrod3", "deepskyblue3"))
+  
+  
