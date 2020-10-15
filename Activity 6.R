55d49d35e4337667267b5d8117b55d05a0931c56
Rachel Pike <rachelpike@rachels-mbp-2.hamilton.edu>
2020-10-15 19:59:48 -0400

    finished activity

--- Activity 6.R
+++ Activity 6.R
@@ -60,4 +60,51 @@ plot(gAll$area66, gAll$gdiff,
      xlab = "Glacier Area in 1966 (m^2)",
      main = "Percent Change in Glacier Area from 1966 to 2015 vs. Glacier Area in 1966")
 
+# Joining data with the spatial data table and overwrite into spatial data table 
+g1966@data <- left_join(g1966@data, gAll, by="GLACNAME")
+# Use spplot to shade polygons based on the % change of labels
+# First argument is the spatial object
+# Second is the column in of data to display with the different colors
+# Add a title using main
+# Col changes the color of the borders. This argument sets them to transparent
+spplot(g1966, "gdiff", main="% change in area", col="transparent")
+
+
+# Calculating the summary statistics for percent change
+mean(gAll$gdiff)
+sd(gAll$gdiff)
+which.min(gAll$gdiff)
+gAll$GLACNAME[25]
+which.max(gAll$gdiff)
+gAll$GLACNAME[5]
+gAll$gdiff[5]
+which.min(gdf66$area66)
+gdf66$GLACNAME[9]
+gdf66$area66[13]
+which.max(gdf66$area66)
+gdf66$GLACNAME[13]
+gAll$gdiff[9]
+gAll$gdiff[13]
+
+# Making a data for the glacier with the largest percent loss
+Boulder66 <- g1966[g1966@data$GLACNAME == "Boulder Glacier",]
+Boulder15 <- g2015[g2015@data$GLACNAME == "Boulder Glacier",]
+
+# Plotting the map of boulder glacier
+plot(Boulder66, col = "deepskyblue4", border = "gray35", 
+     main = "Boulder Glacier Area Loss from 1966 to 2015")
+plot(Boulder15, add = TRUE, col = "lightcyan1", border = "gray35")
+legend("bottomleft", legend=c("1966 Area (meters squared)", "2015 Area (meters squared)"),
+       fill=c( "deepskyblue4", "lightcyan1"), bty="n", cex = 1)
+
+# Making a data for the glacier with the smallest percent loss
+Pumpelly66 <- g1966[g1966@data$GLACNAME == "Pumpelly Glacier",]
+Pumpelly15 <- g2015[g2015@data$GLACNAME == "Pumpelly Glacier",]
+
+# Plotting the map of boulder glacier
+plot(Pumpelly66, col = "deepskyblue4", border = "gray35", 
+     main = "Pumpelly Glacier Area Loss from 1966 to 2015")
+plot(Pumpelly15, add = TRUE, col = "lightcyan", border = "gray35")
+legend("bottomright", legend=c("1966 Area (meters squared)", "2015 Area (meters squared)"),
+       fill=c("lightcyan1", "deepskyblue4"), bty="n", cex = 1)
 
