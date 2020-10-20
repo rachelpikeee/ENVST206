73b0bc6c5f2c2113a11c15ec57dd80c268615de7
Rachel Pike <rachelpike@rachels-mbp-2.hamilton.edu>
2020-10-20 18:27:15 -0400

    did some initial organizing and visualizations

--- Final Project.R
+++ Final Project.R
@@ -4,6 +4,7 @@
 library(tidyr)
 library(lubridate)
 library(dplyr)
+library(ggplot2)
 
 # Reading in the wildfire and drought datasets
 FireData <- read.csv("/Users/rachelpike/ENVST Data/final project/CAfiredata.csv")
@@ -15,10 +16,38 @@ FireData$incident_date_last_update <- as.Date(FireData$incident_date_last_update
 DroughtData$ValidStart <- as.Date(DroughtData$ValidStart, "%Y-%m-%d")
 DroughtData$ValidEnd <- as.Date(DroughtData$ValidEnd, "%Y-%m-%d")
 
-# Adding a year column to help subset the drought data
+# Adding a year column to help subset the both data sets
 DroughtData$Year <- year(DroughtData$ValidStart)
+FireData$Year <- year(FireData$incident_date_created)
+
+# Getting rid of outlier data point in fire data from 1969
+FireData <- subset(FireData, FireData$Year >= 2000)
 
 # Subsetting drought data to match fire data --> earliest date is 2013 for fire
 DroughtData <- subset(DroughtData, DroughtData$Year >= 2013)
 
-# NEXT STEPS: get some initial summary statistics and plots
+# Getting summary statistics for the Fire data area burned
+summary(FireData$incident_acres_burned)
+sd(FireData$incident_acres_burned)
+
+# Getting summary statistics for 2019
+Fire2019 <- subset(FireData, FireData$Year == 2019)
+summary(Fire2019$incident_acres_burned)
+sd(Fire2019$incident_acres_burned)
+
+# Plotting a histogram of the area burned for each fire in 2019
+ggplot(data = Fire2019, aes(x=incident_acres_burned))+
+  geom_histogram(binwidth = 2000, fill = "chocolate", color = "black")+
+  labs(title = "Distribution of Area Burned in 2019",
+       x = "Area Burned (Acres)",
+       y = "Frequency")+
+  theme_bw()
+
+# Creating a violin plot for distributions of area burned for each year
+ggplot(data = FireData, aes(x=Year, y=incident_acres_burned))+
+  geom_count(color="chocolate")+
+  theme_classic()+ # git rid of ugly gridlines
+  labs(title = "Distribution of Fire Sizes Since 2013",
+       x = "Year",
+       y = "Area Burned (Acres)")
+
